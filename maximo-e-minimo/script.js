896d8b6dd28c1f31f782fedfdfd4c488e0bd7448
WevertonBrito <WevertonMB.Dev@gmail.com>
2023-05-22 13:26:46 -0300

    Máximo e Mínimo

---
+++ maximo-e-minimo/script.js
@@ -0,0 +1,27 @@
+function maximo(num1, num2) {
+  if (num1 > num2) {
+    console.log(`O maior número é ${num1}`)
+  } else {
+    console.log(`O maior número é ${num2}`)
+  }
+}
+
+function minimo(num1, num2) {
+  if (num1 < num2) {
+    console.log(`O menor número é ${num1}`)
+  } else {
+    console.log(`O menor número é ${num2}`)
+  }
+}
+
+// function maximo(num1, num2) {
+//   return num1 > num2 ? num1 : num2
+// }
+// function maximo(num1, num2) {
+//   return num1 < num2 ? num1 : num2
+// } Outra maneira mais simples de fazer a mesma coisa
+
+maximo(50, 1)
+minimo(80, 3)
+
+

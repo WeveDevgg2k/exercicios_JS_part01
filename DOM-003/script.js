382472baf6b4e622f04ea8b9ff3afa50de02bb6c
WevertonBrito <WevertonMB.Dev@gmail.com>
2023-06-22 13:54:35 -0300

    Alternando classes

---
+++ DOM-003/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('#btn').addEventListener('click', () => {
+  const div = document.querySelector('.container').classList.toggle('active')
+})
+
+// function changeBgColor() {
+//   const div = document.querySelector('.container')
+
+//   div.classList.toggle('active')
+// }
+
+// const btn = document.querySelector('#btn').addEventListener('click', changeBgColor)
